d611885ece7cf672d3094903a141eec0e11d9f79
GiaBaorikkei <thainguyengiabao082105@gmail.com>
2025-03-05 12:10:13 +0700

    update

--- Session04/src/Bai2.java
+++ Session04/src/Bai2.java
@@ -12,7 +12,7 @@ public class Bai2 {
         } else {
             boolean isPrime = true;
 
-            for (int i = 2; i <= n / 2; i++) {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
                 if (n % i == 0) {
                     isPrime = false;
                     break;
